9623f3cdde9029323c5281d02414f0b75b77886a
tarunrpmahar <Tarun.11608343@lpu.in>
2021-03-23 16:58:46 +0530

    Book Controller and Creating views

---
+++ BookStoreSolution/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+            //return DataSource().Where(x => x.Title == title && x.Author == authorName).ToList();    matches exact word
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id=1, Title= "MVC", Author= "Tarun"},
+                new BookModel(){ Id=2, Title= "Django", Author= "Singh"},
+                new BookModel(){ Id=3, Title= "Machine Learning", Author= "Mahar"},
+                new BookModel(){ Id=4, Title= "C-sharp", Author= "TSM"},
+                new BookModel(){ Id=5, Title= "Dotnet", Author= "tarunrpmahar"},
+            };
+        }
+    }
+}
